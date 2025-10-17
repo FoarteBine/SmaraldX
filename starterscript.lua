a7ce826c5b96a456136331c46cdf60363ab61b98
FoarteBine <sasabelous332@gmail.com>
2025-10-17 23:14:54 +0300

    Create starterscript.lua

---
+++ starterscript.lua
@@ -0,0 +1,22 @@
+
+print([[
+ _____                           _     _  __   __
+/ ____|                         | |   | | \ \ / /
+| (___  _ __ ___   __ _ _ __ __ _| | __| |  \ V / 
+ \___ \| '_ ` _ \ / _` | '__/ _` | |/ _` |   > <  
+ ____) | | | | | | (_| | | | (_| | | (_| |  / . \ 
+|_____/|_| |_| |_|\__,_|_|  \__,_|_|\__,_| /_/ \_\
+]])
+
+warn("Smarald X by Foarte Bine")
+warn("HAHAHHA! ITS WORKING!!!")
+
+
+local StarterGui = game:GetService("StarterGui")
+
+
+StarterGui:SetCore("SendNotification", {
+    Title = "Smarald X",
+    Text = "Yo! Smarald X Injected!! :)",
+    Duration = 5,
+})
